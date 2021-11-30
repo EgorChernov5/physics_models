a2eb224159ae9fbd5d93b108bd621ca4d8ef24b4
EgorChernov5 <EgorChernov3140@gmail.com>
2021-12-01 01:08:06 +0300

    Исправить баг в move_charges()

--- static_q.py
+++ static_q.py
@@ -33,16 +33,54 @@ def init():
     return charges
 
 
-def move_charges(charges, i):
-    for j in range(len(charges)):
-        charges[j] = list(charges[j])
-        charges[j][1] = list(charges[j][1])
-        charges[j][1][0] += i * 0.1
-        charges[j][1][1] += i * 0.1
-        charges[j][1] = tuple(charges[j][1])
-        charges[j] = tuple(charges[j])
-    new_charges = charges
-    return new_charges
+def update_charges(charges):
+    # [[xq1, yq1, q1, mq1, vxq1, vyq1, z]]
+    up_charges = []
+    for charge in charges:
+        q = charge[0]
+        xq, yq = charge[1]
+        mq = 9.1 * 10**(-11)
+        vxq = 0
+        vyq = 0
+        if q == 1:
+            z = 1
+        else:
+            z = 0
+        up_charges.append([xq, yq, q, mq, vxq, vyq, z])
+    return up_charges
+
+
+def initial_charges(up_charges):
+    init_charges = []
+    for up_charge in up_charges:
+        r0 = []
+        q = up_charge[2]
+        r0.extend([up_charge[0], up_charge[1]])
+        init_charges.append([q, r0])
+    return init_charges
+
+
+def move_charges(up_charges):
+    dt = 1
+    new_up_charges = np.copy(up_charges)
+    for c in range(len(up_charges)):  # проход по зарядам и обновление их координат и проекций скоростей
+        if up_charges[c][6] == 1:
+            xs = up_charges[c][0]
+            ys = up_charges[c][1]
+            q = up_charges[c][2]
+            m = up_charges[c][3]
+            vx = up_charges[c][4]
+            vy = up_charges[c][5]
+            Ex, Ey = E_values(up_charges, xs, ys, c)
+            # получается огромный Ex, Ey
+            x = (((Ex * q) / m) * dt ** 2) / 2 + vx * dt + xs
+            y = (((Ey * q) / m) * dt ** 2) / 2 + vy * dt + ys
+            vx += ((Ex * q) / m) * dt
+            vy += ((Ey * q) / m) * dt
+            # print(update_charges[c]-[x,y,q,m,vx,vy])
+            new_up_charges[c] = [x, y, q, m, vx, vy, 1]
+
+    return new_up_charges  # возвращение обновлённого массива характеристик зарядов
 
 
 def vec_el(new_charges):
@@ -54,6 +92,38 @@ def vec_el(new_charges):
         Ex += ex
         Ey += ey
     return Ex, Ey
+
+
+def E_values(up_charges, xs, ys, nq):
+    l = 1
+    k = 9 * 10 ** 9
+    Ex = 0
+    Ey = 0
+    c = 0
+    if nq == None:
+        for c in range(len(up_charges)):
+            q = up_charges[c]
+            r = ((xs - q[0]) ** 2 + (ys - q[1]) ** 2) ** 0.5
+            dEv = (k * q[2]) / r ** 2
+            dEx = (xs - q[0]) * (dEv / r) * l
+            dEy = (ys - q[1]) * (dEv / r) * l
+
+            Ex += dEx
+            Ey += dEy
+        return Ex, Ey
+    else:
+        for c in range(len(up_charges)):
+            if c != nq:
+                q = up_charges[c]
+
+                r = ((xs - q[0]) ** 2 + (ys - q[1]) ** 2) ** 0.5
+                dEv = (k * q[2]) / r ** 2
+                dEx = (xs - q[0]) * (dEv / r) * l
+                dEy = (ys - q[1]) * (dEv / r) * l
+
+                Ex += dEx
+                Ey += dEy
+        return Ex, Ey
 
 
 fig = plt.figure()
@@ -76,22 +146,26 @@ def draw_charges(new_charges):
         ax.add_artist(Circle(pos, 0.05, color=charge_colors[q > 0]))
 
 
-def animate(i):
+def animate():
     ax.clear()
     charges = init()
-    new_charges = move_charges(charges, i)
+    up_charges = update_charges(charges)
+    new_up_charges = move_charges(up_charges)
+    new_charges = initial_charges(new_up_charges)
     Ex, Ey = vec_el(new_charges)
     draw_line(Ex, Ey)
     draw_charges(new_charges)
 
 
-sin_animation = animation.FuncAnimation(fig, animate, frames=10, interval=5, repeat=False)
+animate()
 
-sin_animation.save('anim1.gif', writer='imagemagick', fps=5)
+# sin_animation = animation.FuncAnimation(fig, animate, frames=10, interval=5, repeat=False)
 
-# ax.set_xlabel('$x$')
-# ax.set_ylabel('$y$')
-# ax.set_xlim(-2, 2)
-# ax.set_ylim(-2, 2)
-# ax.set_aspect('equal')
-# plt.show()
+# sin_animation.save('anim1.gif', writer='imagemagick', fps=5)
+
+ax.set_xlabel('$x$')
+ax.set_ylabel('$y$')
+ax.set_xlim(-2, 2)
+ax.set_ylim(-2, 2)
+ax.set_aspect('equal')
+plt.show()
